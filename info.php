4c4d5c95836e8fd6bc38a33e944855a8ec396651
Franziskus Nakajima <franziskus.nakajima@gmail.com>
2016-10-10 22:01:52 +0200

    initial commit

---
+++ info.php
@@ -0,0 +1,11 @@
+<?php
+	$info=
+		array(
+			'name'		=>	'Faris',
+			'designer'	=> 	'Franziskus Nakajima',
+			'website'	=> 	'http://www.franziskusnakajima.net',
+			'date'		=>	'1.0 / February 2016',
+			'about'		=>	'Left menu, right content layout. Lightweight & flexible. Based on Mino.',
+			'preview'	=>	'faris.jpg'
+		);
+?>
